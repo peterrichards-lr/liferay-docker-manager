e79f1fa0cef73747fc5c4e6fa611a0a78a240263
Peter Richards <peter.richards@liferay.com>
2026-04-11 12:44:57 +0100

    chore: bump version to v1.6.37 for final architecture-aware release

--- ldm_core/constants.py
+++ ldm_core/constants.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 # --- Constants & Configuration ---
-# LDM_MAGIC_VERSION: 1.6.36
-VERSION = "1.6.36"
+# LDM_MAGIC_VERSION: 1.6.37
+VERSION = "1.6.37"
 
 BUILD_INFO = None
 IMAGE_NAME_DXP = "liferay/dxp"
