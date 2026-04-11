e9a8ef0015faf9a9bb96f19a8b8d5fb06bfea45d
Peter Richards <peter.richards@liferay.com>
2026-04-11 17:58:01 +0100

    fix(ci): switch back to shiv for macOS and use macos-12 for Intel

--- ldm_core/constants.py
+++ ldm_core/constants.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 # --- Constants & Configuration ---
-# LDM_MAGIC_VERSION: 1.6.42
-VERSION = "1.6.42"
+# LDM_MAGIC_VERSION: 1.6.43
+VERSION = "1.6.43"
 
 BUILD_INFO = None
 IMAGE_NAME_DXP = "liferay/dxp"
