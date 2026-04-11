03e3a3e0a5db644ee51a0b32ab0272cbbcae456f
Peter Richards <peter.richards@liferay.com>
2026-04-11 17:46:23 +0100

    fix(ci): revert Intel runner to macos-13 and bump v1.6.41

--- ldm_core/constants.py
+++ ldm_core/constants.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 # --- Constants & Configuration ---
-# LDM_MAGIC_VERSION: 1.6.40
-VERSION = "1.6.40"
+# LDM_MAGIC_VERSION: 1.6.41
+VERSION = "1.6.41"
 
 BUILD_INFO = None
 IMAGE_NAME_DXP = "liferay/dxp"
