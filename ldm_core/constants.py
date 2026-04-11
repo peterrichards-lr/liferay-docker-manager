a2ef3e98975c1b965baccc7d70c9821a224ffdf2
Peter Richards <peter.richards@liferay.com>
2026-04-11 13:47:10 +0100

    fix(ci): use explicit macos-14 and macos-13 runners to resolve provisioning error

--- ldm_core/constants.py
+++ ldm_core/constants.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 # --- Constants & Configuration ---
-# LDM_MAGIC_VERSION: 1.6.37
-VERSION = "1.6.37"
+# LDM_MAGIC_VERSION: 1.6.38
+VERSION = "1.6.38"
 
 BUILD_INFO = None
 IMAGE_NAME_DXP = "liferay/dxp"
