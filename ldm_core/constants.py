f6c24697fa291afc00ac02906d15ac3dcdc6716c
Peter Richards <peter.richards@liferay.com>
2026-04-14 13:00:48 +0100

    chore: bump version to 1.6.64

--- ldm_core/constants.py
+++ ldm_core/constants.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 # --- Constants & Configuration ---
-# LDM_MAGIC_VERSION: 1.6.63
-VERSION = "1.6.63"
+# LDM_MAGIC_VERSION: 1.6.64
+VERSION = "1.6.64"
 
 BUILD_INFO = None
 IMAGE_NAME_DXP = "liferay/dxp"
