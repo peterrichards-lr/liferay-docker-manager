244cdedaeedc4be46bae35fe1d4ff9b4909eba88
Peter Richards <peter.richards@liferay.com>
2026-04-14 08:16:32 +0100

    chore(release): v1.6.46

--- ldm_core/constants.py
+++ ldm_core/constants.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 # --- Constants & Configuration ---
-# LDM_MAGIC_VERSION: 1.6.45
-VERSION = "1.6.45"
+# LDM_MAGIC_VERSION: 1.6.46
+VERSION = "1.6.46"
 
 BUILD_INFO = None
 IMAGE_NAME_DXP = "liferay/dxp"
