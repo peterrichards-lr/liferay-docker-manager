178255b0bb626394d081304e181cd87b2a9ef947
Peter Richards <peter.richards@liferay.com>
2026-04-14 06:55:29 +0100

    fix(ci): cross-package x86_64 macOS binary on ARM runner to bypass GitHub outage

--- ldm_core/constants.py
+++ ldm_core/constants.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 # --- Constants & Configuration ---
-# LDM_MAGIC_VERSION: 1.6.43
-VERSION = "1.6.43"
+# LDM_MAGIC_VERSION: 1.6.44
+VERSION = "1.6.44"
 
 BUILD_INFO = None
 IMAGE_NAME_DXP = "liferay/dxp"
