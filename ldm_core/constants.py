ccaabfdcb7976107d202a76ea1cc66d7fded3827
Peter Richards <peter.richards@liferay.com>
2026-04-14 08:21:13 +0100

    fix(ci): bump watchdog to 4.0.2 to support Python 3.13 macOS Intel wheels

--- ldm_core/constants.py
+++ ldm_core/constants.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 # --- Constants & Configuration ---
-# LDM_MAGIC_VERSION: 1.6.46
-VERSION = "1.6.46"
+# LDM_MAGIC_VERSION: 1.6.47
+VERSION = "1.6.47"
 
 BUILD_INFO = None
 IMAGE_NAME_DXP = "liferay/dxp"
