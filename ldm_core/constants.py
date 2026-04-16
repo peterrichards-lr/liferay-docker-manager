a318effa013dd7abce32bf2d154dc9250f5b1ead
Peter Richards <peter.richards@liferay.com>
2026-04-16 12:17:45 +0100

    feat(samples): sync updated client extensions and add new assets
    - Synced latest ZIPs from ldm-cx-samples project
    - Added new extensions: ecopulse-brand, veridian-brand, veridian-favicon, veridian-site-initializer
    - Updated reference_tag to 2025.q3.10 to match samples project
    - Bumps version to 1.9.0

--- ldm_core/constants.py
+++ ldm_core/constants.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 # --- Constants & Configuration ---
-# LDM_MAGIC_VERSION: 1.8.9
-VERSION = "1.8.9"
+# LDM_MAGIC_VERSION: 1.9.0
+VERSION = "1.9.0"
 
 BUILD_INFO = None
 IMAGE_NAME_DXP = "liferay/dxp"
