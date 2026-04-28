2da785c4af4004ef0d379cc98227005ec5d6c96c
Peter Richards <peter.richards@liferay.com>
2026-04-28 14:56:41 +0100

    fix: resolve macOS md5 and report loss in verify script [v2.4.26-beta.41]

--- ldm_core/constants.py
+++ ldm_core/constants.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 # --- Constants & Configuration ---
-# LDM_MAGIC_VERSION: 2.4.26-beta.40
-VERSION = "2.4.26-beta.40"
+# LDM_MAGIC_VERSION: 2.4.26-beta.41
+VERSION = "2.4.26-beta.41"
 
 
 BUILD_INFO = None
