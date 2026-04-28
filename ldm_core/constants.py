5ccafee5a6446d6db1d5b0b3ccf927c8d9526265
Peter Richards <peter.richards@liferay.com>
2026-04-28 15:06:55 +0100

    fix: resolve search restore permissions and macOS detection [v2.4.26-beta.42]

--- ldm_core/constants.py
+++ ldm_core/constants.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 # --- Constants & Configuration ---
-# LDM_MAGIC_VERSION: 2.4.26-beta.41
-VERSION = "2.4.26-beta.41"
+# LDM_MAGIC_VERSION: 2.4.26-beta.42
+VERSION = "2.4.26-beta.42"
 
 
 BUILD_INFO = None
