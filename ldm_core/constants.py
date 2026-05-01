8c1fc7c0beb3b7ec99d33990ed326dca63dbbfda
Peter Richards <peter.richards@liferay.com>
2026-05-01 11:04:11 +0100

    fix: Finalize major refactor and cleanup for v2.4.26-pre.8 [pre-release]
    This commit synchronizes the version to v2.4.26-pre.8 and captures the final state of the major deduplication and script cleanup refactor. It ensures that the final pre-release build contains all logic optimizations and a clean repository state.

--- ldm_core/constants.py
+++ ldm_core/constants.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 # --- Constants & Configuration ---
-# LDM_MAGIC_VERSION: 2.4.26-pre.7
-VERSION = "2.4.26-pre.7"
+# LDM_MAGIC_VERSION: 2.4.26-pre.8
+VERSION = "2.4.26-pre.8"
 
 
 BUILD_INFO = None
