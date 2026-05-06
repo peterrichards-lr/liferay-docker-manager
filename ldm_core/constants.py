e206d7e5b3b2e51e48a5ae8ac643846beb37c303
Peter Richards <peter.richards@liferay.com>
2026-05-06 08:52:02 +0100

    chore: bump version to 2.4.27 [release]

--- ldm_core/constants.py
+++ ldm_core/constants.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 # --- Constants & Configuration ---
-# LDM_MAGIC_VERSION: 2.4.26
-VERSION = "2.4.26"
+# LDM_MAGIC_VERSION: 2.4.27
+VERSION = "2.4.27"
 
 
 BUILD_INFO = None
