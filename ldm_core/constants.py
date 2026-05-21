ef1b8ebe3d5cb2c08bee75ddee5fcd67c206bd90
Peter Richards <peter.richards@liferay.com>
2026-05-21 17:41:30 +0100

    chore: finalize stable release v2.7.2
    - Promoted beta.73 to final stable release v2.7.2.
    - Updated internal version constants and pyproject.toml.
    - Synchronized documentation and compatibility matrix to reflect stable status.

--- ldm_core/constants.py
+++ ldm_core/constants.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 # --- Constants & Configuration ---
-# LDM_MAGIC_VERSION: 2.7.2-beta.73
-VERSION = "2.7.2-beta.73"
+# LDM_MAGIC_VERSION: 2.7.2
+VERSION = "2.7.2"
 
 
 BUILD_INFO = None
