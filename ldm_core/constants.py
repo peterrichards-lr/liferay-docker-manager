a388164ce0749ded5b865d51e83ff06155013c3c
Peter Richards <peter.richards@liferay.com>
2026-05-21 16:27:59 +0100

    chore: synchronize internal version strings for beta.72

--- ldm_core/constants.py
+++ ldm_core/constants.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 # --- Constants & Configuration ---
-# LDM_MAGIC_VERSION: 2.7.2-beta.70
-VERSION = "2.7.2-beta.70"
+# LDM_MAGIC_VERSION: 2.7.2-beta.72
+VERSION = "2.7.2-beta.72"
 
 
 BUILD_INFO = None
