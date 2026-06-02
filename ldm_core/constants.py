0145babe39ceb6aba6d487c807ccb0faaa293d2e
Peter Richards <peter.richards@liferay.com>
2026-06-02 07:33:36 +0100

    chore: release stable v2.10.55 with codifed Pre-Release Strategy
    * Formally integrated the Pre-Release Strategy mandates into the release channel.
    
    * This version serves as the stable baseline for future experimental work.
    
    [release]

--- ldm_core/constants.py
+++ ldm_core/constants.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 # --- Constants & Configuration ---
-# LDM_MAGIC_VERSION: 2.10.54
-VERSION = "2.10.54"
+# LDM_MAGIC_VERSION: 2.10.55
+VERSION = "2.10.55"
 
 
 BUILD_INFO = None
