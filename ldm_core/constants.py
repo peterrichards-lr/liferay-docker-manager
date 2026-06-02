5f5ea6451baeb435bef224d1b03079a2e7688807
Peter Richards <peter.richards@liferay.com>
2026-06-02 09:24:05 +0100

    fix(ci): configure shellcheck to use system binary and remove docker pull

--- ldm_core/constants.py
+++ ldm_core/constants.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 # --- Constants & Configuration ---
-# LDM_MAGIC_VERSION: 2.10.57
-VERSION = "2.10.57"
+# LDM_MAGIC_VERSION: 2.10.58
+VERSION = "2.10.58"
 
 
 BUILD_INFO = None
