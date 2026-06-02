11c37581c050309acb974d09bce49389efe9a4d9
Peter Richards <peter.richards@liferay.com>
2026-06-02 10:21:16 +0100

    chore: release stable v2.10.59
    * Increments version to v2.10.59.
    
    [release]

--- ldm_core/constants.py
+++ ldm_core/constants.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 # --- Constants & Configuration ---
-# LDM_MAGIC_VERSION: 2.10.58
-VERSION = "2.10.58"
+# LDM_MAGIC_VERSION: 2.10.59
+VERSION = "2.10.59"
 
 
 BUILD_INFO = None
