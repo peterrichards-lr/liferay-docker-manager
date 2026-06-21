27085ff5772f6c907b085c2a9cc8b0a8ce3eea6a
Peter Richards <80279709+peterrichards-lr@users.noreply.github.com>
2026-06-21 08:56:02 +0100

    chore: bump version to 2.11.24 (#88)

--- ldm_core/constants.py
+++ ldm_core/constants.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 # --- Constants & Configuration ---
-# LDM_MAGIC_VERSION: 2.11.23
-VERSION = "2.11.23"
+# LDM_MAGIC_VERSION: 2.11.24
+VERSION = "2.11.24"
 
 
 BUILD_INFO = None
