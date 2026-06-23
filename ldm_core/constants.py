8f69839835a6f9a29f58fe8221ae73afc5621a8a
Peter Richards <peter.richards@liferay.com>
2026-06-23 14:48:14 +0100

    chore(release): sync package version to 2.11.36 and optimize smoke-test conditional

--- ldm_core/constants.py
+++ ldm_core/constants.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 # --- Constants & Configuration ---
-# LDM_MAGIC_VERSION: 2.11.35
-VERSION = "2.11.35"
+# LDM_MAGIC_VERSION: 2.11.36
+VERSION = "2.11.36"
 # Release commit v2.11.30
 
 
