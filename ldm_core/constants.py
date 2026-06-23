31128048d81cb761280dc248cea3908e3b67e905
Peter Richards <80279709+peterrichards-lr@users.noreply.github.com>
2026-06-23 21:06:48 +0100

    chore(release): bump version to v2.11.41 [release] (#119)
    * fix(test): unset GITHUB_ACTIONS for E2E project collision test
    
    * chore(release): bump version to v2.11.41 [release]

--- ldm_core/constants.py
+++ ldm_core/constants.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 # --- Constants & Configuration ---
-# LDM_MAGIC_VERSION: 2.11.40
-VERSION = "2.11.40"
+# LDM_MAGIC_VERSION: 2.11.41
+VERSION = "2.11.41"
 # Release commit v2.11.30
 
 
