3c4518eb1d1f550a2d32d5bb4085b3cc4ff3c404
Peter Richards <80279709+peterrichards-lr@users.noreply.github.com>
2026-06-24 10:34:38 +0100

    chore(release): bump version to v2.11.46 [release] (#138)

--- ldm_core/constants.py
+++ ldm_core/constants.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 # --- Constants & Configuration ---
-# LDM_MAGIC_VERSION: 2.11.45
-VERSION = "2.11.45"
+# LDM_MAGIC_VERSION: 2.11.46
+VERSION = "2.11.46"
 # Release commit v2.11.30
 
 
