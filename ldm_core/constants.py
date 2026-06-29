8b8baf985593dd4ca1fd226d6b93c8f8af12e62f
Peter Richards <80279709+peterrichards-lr@users.noreply.github.com>
2026-06-29 13:13:27 +0100

    chore(release): bump version to v2.11.63 [release] (#215)
    * docs: Document Liferay version upgrades and new database upgrade flags
    
    * chore(release): bump version to v2.11.63 [release]

--- ldm_core/constants.py
+++ ldm_core/constants.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 # --- Constants & Configuration ---
-# LDM_MAGIC_VERSION: 2.11.62
-VERSION = "2.11.62"
+# LDM_MAGIC_VERSION: 2.11.63
+VERSION = "2.11.63"
 # Release commit v2.11.30
 
 
