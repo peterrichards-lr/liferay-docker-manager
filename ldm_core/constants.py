bff61ee7436550ea18fa3016d76b3f456bee49f3
Peter Richards <80279709+peterrichards-lr@users.noreply.github.com>
2026-07-14 12:08:32 +0100

    release: v2.15.16 (#567)
    * ci: upgrade setuptools during dependencies install to fix pip-audit vulnerability check
    
    * chore(release): promote version to v2.15.16 [release]

--- ldm_core/constants.py
+++ ldm_core/constants.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 # --- Constants & Configuration ---
-# LDM_MAGIC_VERSION: 2.15.16-pre.11
-VERSION = "2.15.16-pre.11"
+# LDM_MAGIC_VERSION: 2.15.16
+VERSION = "2.15.16"
 # Release commit v2.11.30
 
 
