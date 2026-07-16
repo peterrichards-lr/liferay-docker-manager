8cdee1d53d5b30af738fd7da03651178eda1de20
Peter Richards <peter.richards@liferay.com>
2026-07-16 12:40:05 +0100

    chore(version): advance to pre.8 to bypass remote tag conflict

--- ldm_core/constants.py
+++ ldm_core/constants.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
 # --- Constants & Configuration ---
-# LDM_MAGIC_VERSION: 2.15.18-pre.7
-VERSION = "2.15.18-pre.7"
+# LDM_MAGIC_VERSION: 2.15.18-pre.8
+VERSION = "2.15.18-pre.8"
 
 # Release commit v2.11.30
 
