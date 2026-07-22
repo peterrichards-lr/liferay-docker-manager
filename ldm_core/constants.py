4325e2d90441ff9bdcc258c0af7564ff84f7058a
Peter Richards <peter.richards@liferay.com>
2026-07-22 22:09:09 +0100

    chore(release): bump version to v2.15.22-pre.7 [release]

--- ldm_core/constants.py
+++ ldm_core/constants.py
@@ -2,8 +2,8 @@ import os
 from pathlib import Path
 
 # --- Constants & Configuration ---
-# LDM_MAGIC_VERSION: 2.15.22-pre.6
-VERSION = "2.15.22-pre.6"
+# LDM_MAGIC_VERSION: 2.15.22-pre.7
+VERSION = "2.15.22-pre.7"
 
 # Release commit v2.11.30
 
