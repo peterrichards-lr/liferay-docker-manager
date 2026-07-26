82baf54bee0789cb4347c1caf79112f0b7b4bbb5
Peter Richards <peter.richards@liferay.com>
2026-07-26 12:06:48 +0100

    fix(tray): remove duplicate autostart methods

--- ldm_core/gui/tray.py
+++ ldm_core/gui/tray.py
@@ -320,23 +320,6 @@ class LdmTrayApp:
         yield pystray.Menu.SEPARATOR
         yield MenuItem("Quit LDM Tray", self.on_quit)
 
-    def is_autostart_enabled(self) -> bool:
-        try:
-            return self.manager.tray.is_autostart_enabled()
-        except Exception:
-            return False
-
-    def on_toggle_autostart(self, icon, item):
-        try:
-            if self.is_autostart_enabled():
-                self.manager.tray.remove_autostart()
-            else:
-                self.manager.tray.setup_autostart()
-        except Exception as e:
-            from ldm_core.ui import UI
-
-            UI.error(f"Failed to toggle autostart: {e}")
-
     def get_menu(self):
         return pystray.Menu(self._menu_generator)
 
