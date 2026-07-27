ddc733ddd9fef2f99b2d4a0775f52a4b6e518bdc
Peter Richards <80279709+peterrichards-lr@users.noreply.github.com>
2026-07-27 14:29:32 +0100

    chore(release): bump version to v2.15.23-pre.1 [release] (#844)

--- ldm_core/constants.py
+++ ldm_core/constants.py
@@ -2,8 +2,8 @@ import os
 from pathlib import Path
 
 # --- Constants & Configuration ---
-# LDM_MAGIC_VERSION: 2.15.22
-VERSION = "2.15.22"
+# LDM_MAGIC_VERSION: 2.15.23-pre.1
+VERSION = "2.15.23-pre.1"
 
 # Release commit v2.11.30
 
