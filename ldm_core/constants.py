2fb5207fd658b379ad16ed9b583632c9244dc45e
Peter Richards <peter.richards@liferay.com>
2026-08-03 12:05:48 +0100

    chore(version): set master base version to stable v2.15.25

--- ldm_core/constants.py
+++ ldm_core/constants.py
@@ -2,8 +2,8 @@ import os
 from pathlib import Path
 
 # --- Constants & Configuration ---
-# LDM_MAGIC_VERSION: 2.15.25-pre.14
-VERSION = "2.15.25-pre.14"
+# LDM_MAGIC_VERSION: 2.15.25
+VERSION = "2.15.25"
 
 # Release commit v2.11.30
 
