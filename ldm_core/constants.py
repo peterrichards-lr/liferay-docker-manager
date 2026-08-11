4f547f703c9ad0ad721e8da8e1399fb6f2e65ebc
Peter Richards <peter.richards@liferay.com>
2026-08-11 16:01:16 +0100

    chore(release): promote version to v2.15.27 [release]

--- ldm_core/constants.py
+++ ldm_core/constants.py
@@ -2,8 +2,8 @@ import os
 from pathlib import Path
 
 # --- Constants & Configuration ---
-# LDM_MAGIC_VERSION: 2.15.27-pre.4
-VERSION = "2.15.27-pre.4"
+# LDM_MAGIC_VERSION: 2.15.27
+VERSION = "2.15.27"
 
 # Release commit v2.11.30
 
