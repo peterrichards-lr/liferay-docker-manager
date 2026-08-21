dbcab22062f9a1d0e3d71d86c283f84509691bb7
Peter Richards <peter.richards@liferay.com>
2026-08-21 19:20:29 +0100

    chore(release): bump version to v2.15.33-pre.1 [release]

--- ldm_core/constants.py
+++ ldm_core/constants.py
@@ -2,8 +2,8 @@ import os
 from pathlib import Path
 
 # --- Constants & Configuration ---
-# LDM_MAGIC_VERSION: 2.15.32
-VERSION = "2.15.32"
+# LDM_MAGIC_VERSION: 2.15.33-pre.1
+VERSION = "2.15.33-pre.1"
 
 # Release commit v2.11.30
 
