be48f4a24847c1f0c95a23e97f25f5b53333d617
Peter Richards <80279709+peterrichards-lr@users.noreply.github.com>
2026-08-28 18:23:49 +0100

    chore(release): release tracking PR for v2.18.0 [release] (#1387)
    * chore(release): bump version to v2.18.0-pre.1 [release]
    
    * fix(verify): drop the backslash escape that broke the shared-db block on Linux (Closes #1388) (#1389)
    
    * chore(release): bump version to v2.18.0-pre.2 [release]
    
    * chore(release): bump version to v2.18.0-pre.3 [release]
    
    * chore(release): bump version to v2.18.0-pre.4 [release]
    
    * chore(release): bump version to v2.18.0-pre.5 [release]
    
    * chore(release): bump version to v2.18.0-pre.6 [release]
    
    * chore(release): bump version to v2.18.0-pre.7 [release]
    
    * chore(release): bump version to v2.18.0-pre.8 [release]
    
    * chore(release): bump version to v2.18.0-pre.9 [release]
    
    * chore(release): bump version to v2.18.0-pre.10 [release]
    
    * chore(release): bump version to v2.18.0-pre.11 [release]
    
    * chore(release): promote version to v2.18.0 [release]

--- ldm_core/constants.py
+++ ldm_core/constants.py
@@ -2,8 +2,8 @@ import os
 from pathlib import Path
 
 # --- Constants & Configuration ---
-# LDM_MAGIC_VERSION: 2.17.0
-VERSION = "2.17.0"
+# LDM_MAGIC_VERSION: 2.18.0
+VERSION = "2.18.0"
 
 # Release commit v2.11.30
 
